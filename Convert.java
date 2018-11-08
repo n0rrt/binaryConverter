ee2e2afeb760ff4d646683aa120111a38b1b19bd
Tim Harris <timthebananalord@gmail.com>
2018-11-08 15:57:57 -0500

    changed removeChar method

--- Convert.java
+++ Convert.java
@@ -8,15 +8,16 @@ public class Convert {
 	static int x;
 	int remainder = (x / 2);
 	static ArrayList<Integer> binArr = new ArrayList<Integer>();
-	
+	static char[] badChars = { ',', ' ', '[', ']' };
+	static Character empty = null;
+
 	public static void convert() {
 		Scanner scan = new Scanner(System.in);
 		x = scan.nextInt();
-		while (x>1) {
+		while (x > 1) {
 			if (x % 2 == 1) {
 				binArr.add(0, 1);
-			} 
-			else {
+			} else {
 				binArr.add(0, 0);
 			}
 			x = x / 2;
@@ -24,24 +25,33 @@ public class Convert {
 		if (x == 1) {
 			binArr.add(0, 1);
 		}
-		String output=Arrays.toString(binArr.toArray());
-		output= removeChar(output, '[');
-		output= removeChar(output, ',');
-		output= removeChar(output, ' ');
-		output= removeChar(output, ']');
+		String output = Arrays.toString(binArr.toArray());
+		output = removeChar(output);
 		System.out.println(output);
 	}
 
-	public static String removeChar(String s, char c) {
-
+	public static String removeChar(String s) {
 		String r = "";
+		for (char c: s.toCharArray()) {
+			if (!contains(badChars, c)) {
+				r+=c;
+			}
+		}
+		return r;
+	}
 
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) != c)
-				r += s.charAt(i);
+	public static boolean contains(final char[] array, final char v) {
+
+		boolean result = false;
+
+		for (char i : array) {
+			if (i == v) {
+				result = true;
+				break;
+			}
 		}
 
-		return r;
+		return result;
 	}
 
 	public static void main(String args[]) {
